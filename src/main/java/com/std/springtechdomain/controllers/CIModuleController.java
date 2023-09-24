b25f3b0be66ec966e29a3f95c8c793e3a99cfa1c
cptlazy1 <o.culhaci@gmail.com>
2023-09-24 17:19:16 +0200

    minor changes

--- src/main/java/com/std/springtechdomain/controllers/CIModuleController.java
+++ src/main/java/com/std/springtechdomain/controllers/CIModuleController.java
@@ -1,4 +1,19 @@
 package com.std.springtechdomain.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cimodules")
 public class CIModuleController {
+
+    // GetMapping
+
+    // GetMapping with {index}
+
+    // PostMapping
+
+    // PutMapping
+
+    // DeleteMapping
 }
--- src/main/java/com/std/springtechdomain/controllers/RemoteControllerController.java
+++ src/main/java/com/std/springtechdomain/controllers/RemoteControllerController.java
@@ -1,4 +1,19 @@
 package com.std.springtechdomain.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remotecontrollers")
 public class RemoteControllerController {
+
+    // GetMapping
+
+    // GetMapping with {index}
+
+    // PostMapping
+
+    // PutMapping
+
+    // DeleteMapping
 }
